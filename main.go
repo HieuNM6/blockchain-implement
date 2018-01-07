d38456adead06505cf7b7381c51bb5909f7cdd46
Hieu Nguyen <hieu.nguyen@mobiclixgroup.com>
2018-01-07 16:24:19 +0700

    add feature validate by pow

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"log"
+	"strconv"
 )
 
 func main() {
@@ -17,6 +18,8 @@ func main() {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("Pow:  %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
