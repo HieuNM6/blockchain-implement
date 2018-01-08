2fac621816e4a222a69c6321f9ccf48d29580907
Hieu Nguyen <hieu.nguyen@mobiclixgroup.com>
2018-01-08 14:45:40 +0700

    add cli and persistence using boltDB

--- block.go
+++ block.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"log"
 	"strconv"
 	"time"
 )
@@ -16,13 +18,6 @@ type Block struct {
 	Nonce         int
 }
 
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-	b.Hash = hash[:]
-}
-
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	b := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
 	pow := NewProofOfWork(b)
@@ -32,4 +27,34 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 	b.Nonce = nonce
 
 	return b
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		return nil
+	}
+
+	return &block
 }
--- main.go
+++ main.go
@@ -3,25 +3,19 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
 	"log"
-	"strconv"
 )
 
 func main() {
 	bc := NewBlockchain()
+	defer func() {
+		if err := bc.db.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
 
-	bc.AddBlock("Send 1 BTC to Hieu")
-	bc.AddBlock("Send 2 more BTC to Hieu")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("Pow:  %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
 
 func IntToHex(num int64) []byte {
